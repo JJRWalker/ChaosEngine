8963aee63ddb35ab1c027eaf6512cef7189d1942
JJRWalker <josh.walker@outlook.com>
2020-04-20 19:37:00 +0100

    added files for vulkan renderer, previously forgotten

---
+++ ChaosEngine/src/Chaos/Core/Scene.cpp
@@ -0,0 +1,10 @@
+#include "chaospch.h"
+#include "Scene.h"
+
+namespace Chaos
+{
+	void Scene::StartScene()
+	{
+
+	}
+}
---
+++ ChaosEngine/src/Chaos/Core/Scene.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "chaospch.h"
+#include "Chaos/Renderer/Texture.h"
+
+namespace Chaos
+{
+	class Scene
+	{
+	public:
+		void StartScene();
+	};
+}
