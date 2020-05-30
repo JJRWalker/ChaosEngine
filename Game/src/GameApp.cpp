405997358dc012f2134cd731b2f270b4c0b731fb
JJRWalker <jjrwalkerdev@gmail.com>
2020-05-30 15:11:28 +0100

    commented GameApp class to show functionality

--- Game/src/GameApp.cpp
+++ Game/src/GameApp.cpp
@@ -3,6 +3,9 @@
 #include <Chaos/Events/KeyEvent.h>
 #include <Chaos/Components/Camera.h>
 
+
+//NOTE: this is a quick and dirty implementation to test functionality, not representitive of the product
+
 class ExampleLayer : public Chaos::Layer
 {
 public:
@@ -12,29 +15,23 @@ public:
 	float moveSpeed = 1.f;
 	int xDir = 0;
 	int yDir = 0;
+
+	//Defining textures
 	Chaos::Ref<Chaos::Texture> player = Chaos::Texture::Create("../Game/textures/sprite-test.png");
-	Chaos::Ref<Chaos::SubTexture> playersub = Chaos::SubTexture::Create(player, Chaos::Vec2(1, 1), Chaos::Vec2(320, 320));
+	Chaos::Ref<Chaos::SubTexture> playersub = Chaos::SubTexture::Create(player, Chaos::Vec2(1, 1), Chaos::Vec2(160, 160));
 	Chaos::Ref<Chaos::Texture> floor = Chaos::Texture::Create("../Game/textures/Floor.jpg");
 	Chaos::Ref<Chaos::Texture> test = Chaos::Texture::Create("../Game/textures/test.png");
-	//Chaos::Ref<Chaos::Texture> big = Chaos::Texture::Create("../Game/textures/big-texture.png", 1);
 	Chaos::Ref<Chaos::Texture> blank = Chaos::Texture::Create("");
 	Chaos::Ref<Chaos::Texture> test2 = Chaos::Texture::Create("../Game/textures/test2.png");
 	float x = 0;
 	float y = 0;
 
-	Chaos::Camera& cam1 = Chaos::Camera(Chaos::Vec3::Zero(), Chaos::Vec3::Zero(), Chaos::Vec4(-5, 5, -5, 5));
-	Chaos::Camera& cam2 = Chaos::Camera(Chaos::Vec3::Zero(), Chaos::Vec3::Zero(), Chaos::Vec4(-5, 5, -5, 5));
-	Chaos::Camera& currentCam = cam1;
-
+	//reference to renderer to be abstracted
 	Chaos::Renderer& renderer = Chaos::Application::Get().GetRenderer();
-
-	bool pressed = false;
-	bool switched = false;
 
 	void OnUpdate(float deltaTime) override
 	{
-		
-
+		//Inefficent input checking, TODO: change to axis based input system / expand
 		if (Chaos::Input::IsKeyPressed(KEY_W))
 		{
 			yDir = 1;
@@ -61,67 +58,45 @@ public:
 			xDir = 0;
 		}
 
-		if (Chaos::Input::IsKeyPressed(KEY_E) && pressed == false)
-		{
-			if (switched)
-			{
-				Chaos::Application::Get().GetMainCamera() = cam1;
-				x = cam1.GetPosition().X;
-				y = cam1.GetPosition().Y;
-				switched = false;
-			}
-			else
-			{
-				Chaos::Application::Get().GetMainCamera() = cam2;
-				x = cam2.GetPosition().X;
-				y = cam2.GetPosition().Y;
-				switched = true;
-			}
-			pressed = true;
-		}
-		else if (Chaos::Input::IsKeyReleased(KEY_E) && pressed == true)
-		{
-			pressed = false;
-		}
+		//Delta time is passed to each layer by the Application class 
 		x += xDir * (moveSpeed * deltaTime);
 		y += yDir * (moveSpeed * deltaTime);
 
-		//Stress test 
+		//Stress test
+		//Inefficient, these draw calls should be made on the start of the scene and stored in a static buffer (one that isn't recreated every frame) as they do not move
 		/*
 		for (int x = 0; x < 100; ++x)
 		{			
 			for (int y = 0; y < 100; ++y)
 			{
-				//Chaos::Timer timer("GameApp: StressTest");
+				//Chaos::Timer timer("GameApp: StressTest"); //Timer example, logs to the console when it leaves scope
 				renderer.DrawQuad(Chaos::Vec2(x - 20, y - 20), Chaos::Vec2(1.f, 1.f), floor);
 			}
 		}
 		*/
 		
+		//Tiling example
+		renderer.DrawQuad(Chaos::Vec2(0.f, 0.f), Chaos::Vec2(20.f, 20.f), floor, 30); //this one tiles
+		renderer.DrawQuad(Chaos::Vec2(0.f, 0.f), Chaos::Vec2(1.f, 1.f), floor);	//this one does not
 
-		renderer.DrawQuad(Chaos::Vec2(0.f, 0.f), Chaos::Vec2(20.f, 20.f), floor, 30);
-		renderer.DrawQuad(Chaos::Vec2(0.f, 0.f), Chaos::Vec2(1.f, 1.f), floor);
-		renderer.DrawQuad(Chaos::Vec2(4.f, 0.f), Chaos::Vec2(1.f, 1.f), playersub);
-		renderer.DrawQuad(Chaos::Vec2(3.f, 0.5f), Chaos::Vec2(1.f, 1.f), Chaos::Vec4(0.7f, 0.7f, 0.1f, 0.4f), blank);
-		//renderer.DrawQuad(Chaos::Vec2(3.f, 0.f), Chaos::Vec2(1.f, 1.f), Chaos::Vec4(0.1,1,0.1,1), blank);
-		//renderer.DrawQuad(Chaos::Vec2(2.f, 0.f), Chaos::Vec2(1.f, 1.f), test2);
-		renderer.DrawQuad(Chaos::Vec2(0, 0), Chaos::Vec2(1.f, 1.f), Chaos::Vec4(1.0f, 0.1f, 0.1f, 0.9f), player);
+		//Coloured sprite example
+		renderer.DrawQuad(Chaos::Vec2(0, 0), Chaos::Vec2(1.f, 1.f), Chaos::Vec4(1.0f, 0.1f, 0.1f, 0.9f), player);	//tinted red 
 
-		Chaos::Application::Get().GetMainCamera().SetPosition(Chaos::Vec3( x, y, 0.f));
+		//sub sprite example
+		renderer.DrawQuad(Chaos::Vec2(4.f, 0.f), Chaos::Vec2(1.f, 1.f), playersub);	//draws the bottom right quarter of the player sprite
 
-		//LOGTRACE("x:{0} y:{1} z:{2}", Chaos::Application::Get().GetMainCamera().GetPosition().X, Chaos::Application::Get().GetMainCamera().GetPosition().Y, Chaos::Application::Get().GetMainCamera().GetPosition().Z);
+		//Camera movement example (not fully implemented)
+		Chaos::Application::Get().GetMainCamera().SetPosition(Chaos::Vec3( x, y, 0.f));	// moves camera based on directional input from W A S D keys 
 	}
 
 	void OnAttach() override
 	{
+		//Stuff to do on start goes here
 	}
 
 	void OnEvent(Chaos::Event& event) override
 	{
-		if (event.GetEventType() == Chaos::EventType::KeyPressed)
-		{
-
-		}
+		//On every window event
 	}
 };
 
