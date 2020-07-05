8bc062edb70b94632e698b7799bb58e201f49dcc
JJRWalker <jjrwalkerdev@gmail.com>
2020-07-05 19:21:24 +0100

    Cleaned up Input
    Input now only uses the one header for input and then is implemented in a platform specific cpp file. This cpp file should only be compiled when that platform is selected for building

--- ChaosEngine/src/Chaos/Input/Input.h
+++ ChaosEngine/src/Chaos/Input/Input.h
@@ -12,30 +12,14 @@ namespace Chaos
 	public:
 		//STATIC DEFINITIONS
 		//KEYS
-		inline static bool IsKeyPressed(KeyCode key) { return s_instance->IsKeyPressedImpl(key); }
-		inline static bool IsKeyReleased(KeyCode key) { return s_instance->IsKeyReleasedImpl(key); }
+		static bool IsKeyPressed(KeyCode key);
+		static bool IsKeyReleased(KeyCode key);
 
 		//MOUSE
-		inline static bool IsMouseButtonPressed(MouseCode button) { return s_instance->IsMouseButtonPressedImpl(button); }
-		inline static bool IsMouseButtonReleased(MouseCode button) { return s_instance->IsMouseButtonReleasedImpl(button); }
-		inline static std::pair<float, float> GetMousePosition() { return s_instance->GetMousePositionImpl(); }
-		inline static float GetMouseX() { return s_instance->GetMouseXImpl(); }
-		inline static float GetMouseY() { return s_instance->GetMouseYImpl(); }
-	protected:
-		//IMPLEMENTATIONS
-		//KEYS
-		virtual bool IsKeyPressedImpl(KeyCode key) = 0;
-		virtual bool IsKeyReleasedImpl(KeyCode key) = 0;
-
-		//MOUSE
-		virtual bool IsMouseButtonPressedImpl(MouseCode button) = 0;
-		virtual bool IsMouseButtonReleasedImpl(MouseCode button) = 0;
-		virtual std::pair<float, float> GetMousePositionImpl() = 0;
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-
-
-	private:
-		static Input* s_instance;
+		static bool IsMouseButtonPressed(MouseCode button);
+		static bool IsMouseButtonReleased(MouseCode button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	};
 }
--- ChaosEngine/src/Platform/Windows/WindowsInput.cpp
+++ ChaosEngine/src/Platform/Windows/WindowsInput.cpp
@@ -1,43 +1,41 @@
+//This file should only be included on windows builds, if it is a window build, generic definitions in Input will look up / be defined here
 #include "chaospch.h"
-#include "WindowsInput.h"
-
+#include "Chaos/Input/Input.h"
 
 #include "Chaos/Core/Application.h"
 #include <GLFW/glfw3.h>
 
 namespace Chaos
 {
-	Input* Input::s_instance = new WindowsInput();
-
-	bool WindowsInput::IsKeyPressedImpl(KeyCode key)
+	bool Input::IsKeyPressed(KeyCode key)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, static_cast<int32_t>(key));
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
-	bool WindowsInput::IsKeyReleasedImpl(KeyCode key)
+	bool Input::IsKeyReleased(KeyCode key)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, static_cast<int32_t>(key));
 		return state == GLFW_RELEASE;
 	}
 
-	bool WindowsInput::IsMouseButtonPressedImpl(MouseCode button)
+	bool Input::IsMouseButtonPressed(MouseCode button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
 		return state == GLFW_PRESS;
 	}
 
-	bool WindowsInput::IsMouseButtonReleasedImpl(MouseCode button)
+	bool Input::IsMouseButtonReleased(MouseCode button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
 		return state == GLFW_RELEASE;
 	}
 
-	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	std::pair<float, float> Input::GetMousePosition()
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double xPos, yPos;
@@ -45,15 +43,15 @@ namespace Chaos
 		return { (float)xPos, (float)yPos };
 	}
 
-	float WindowsInput::GetMouseXImpl()
+	float Input::GetMouseX()
 	{
-		auto [x, y] = GetMousePositionImpl();
+		auto [x, y] = GetMousePosition();
 		return (float)x;
 	}
 
-	float WindowsInput::GetMouseYImpl()
+	float Input::GetMouseY()
 	{
-		auto [x, y] = GetMousePositionImpl();
+		auto [x, y] = GetMousePosition();
 		return (float)y;
 	}
 }
