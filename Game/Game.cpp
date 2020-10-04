c882e5f97758e0e73e0331d091a888960c692402
JJRWalker <jjrwalkerdev@gmail.com>
2020-10-04 12:42:15 +0100

    Cleaned Game project
    - Cleaned out the game project, no longer includes an example, examples are stored on seperate branches.

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,10 +1,3 @@
 //unity build file for easy compilation outside of visual studio
 #pragma once
 #include <GameApp.cpp>
-#include <Components/PlayerAnimatorController.cpp>
-#include <Components/PlayerController.cpp>
-#include <Entities/Player.cpp>
-
-//========ANIMATION STATES==========================
-#include <AnimationStates/PlayerAnimation_IdleState.cpp>
-#include <AnimationStates/PlayerAnimation_RunningState.cpp>
--- Game/src/GameApp.cpp
+++ Game/src/GameApp.cpp
@@ -1,125 +1,38 @@
 #pragma once
 #include <Chaos.h>
-#include <Chaos/Events/KeyEvent.h>
 #include <Chaos/Core/EntryPoint.h>
-#include "Components/PlayerController.h"
-#include "Entities/Player.h"
-#include "Chaos/Entity/Components/CellularAutomata.h"
-#include <Chaos/Entity/Components/Camera.h>
 
-//NOTE: this is a quick and dirty implementation to test functionality, not representitive of the product
+//Template for a fresh project
 
 class ExampleLayer : public Chaos::Layer
 {
 	public:
 	ExampleLayer()
-		:Layer("Example") {}
+		:Layer("Example") {};
 	
-	//Test entity
-	Chaos::Player player;
-	
-	//Ray
-	Chaos::RayHit2DInfo* rayHitInfo;
-	
-	Chaos::Vec2 line = Chaos::Vec2(0.0f, 1.0f);
-	
-	static const int MAP_SIZE_X = 30;
-	static const int MAP_SIZE_Y = 30;
-	//char m_map[MAP_SIZE_X][MAP_SIZE_Y] = {
-	//										'w', 'w', 'w', 'w' ,'w', 'w', 'w', 'w', 'w', 'w' ,'w', 'w', 'w', 'w', 'w', 'w' ,'w', 'w', 'w', 'w', 
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', 'w', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', 'w', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', 'w', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', 'w', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', '0', '0', '0' ,'0', '0', '0', 'w',
-	//										'w', 'w', 'w', 'w' ,'w', 'w', 'w', 'w', 'w', 'w' ,'w', 'w', 'w', 'w', 'w', 'w' ,'w', 'w', 'w', 'w'
-	//
-	//									};
-	char m_map[MAP_SIZE_X][MAP_SIZE_Y];
-	
-	//Raycaster
-	std::vector<Chaos::Entity*> raycastDisplay;
-	const int RESOLUTION = 60;
-	const float FOV = 30;
-	const float SIGHT_DISTANCE = 20.0f;
-	const float COLOUR_DROPOFF = 10.0f;
 	
 	void OnUpdate(float deltaTime) override
 	{
-		//Chaos::Application::Get().GetRenderer().DrawQuad(Chaos::Vec3(0, 0, 1), Chaos::Vec2(1, 1), Chaos::SubTexture::Create("../Game/textures/floor.jpg", Chaos::Vec2(0, 0), Chaos::Vec2(16, 16)));
+		//called every frame
 	}
 	
 	void OnAttach() override
 	{
-		Chaos::SceneManager::GetScene()->StartScene();
-		player.GetPosition() = Chaos::Vec3(0, 0, 1);
-		player.GetScale() = Chaos::Vec2(2, 2);
+		//called when this layer is attached to the layer stack
 	}
 	
 	void OnDetach() override
 	{
+		//called when removed from layer stack, use to free resources
 	}
 	
 	void OnEvent(Chaos::Event& event) override
 	{
 		//On every window event
 	}
-	
-	void GenerateLevel()
-	{
-		for (int xpos = 0; xpos < MAP_SIZE_X; ++xpos)
-		{
-			for (int ypos = 0; ypos < MAP_SIZE_Y; ++ypos)
-			{
-				if ((xpos == 0 || xpos == MAP_SIZE_X - 1 || ypos == 0 || ypos == MAP_SIZE_Y - 1) || ((xpos < 8 && ypos < 4) || (xpos > 22 && ypos > 22)))
-				{
-					m_map[xpos][ypos] = 'w';
-				}
-			}
-		}
-		
-		for (int xpos = 0; xpos < MAP_SIZE_X; ++xpos)
-		{
-			for (int ypos = 0; ypos < MAP_SIZE_Y; ++ypos)
-			{
-				if (m_map[xpos][ypos] == 'w')
-				{
-					Chaos::Entity* wall = new Chaos::Entity("Wall");
-					wall->AddComponent<Chaos::BoxCollider2D>();
-					wall->GetTransform()->Position() = Chaos::Vec3(xpos, ypos, 0.f);
-				}
-			}
-		}
-		
-		//setting up level walls
-		for (int xpos = 0; xpos < MAP_SIZE_X; ++xpos)
-		{
-			for (int ypos = 0; ypos < MAP_SIZE_Y; ++ypos)
-			{
-				if (m_map[xpos][ypos] == 'w')
-				{
-					Chaos::Entity* wall = new Chaos::Entity("Wall");
-					wall->AddComponent<Chaos::BoxCollider2D>();
-					wall->GetTransform()->Position() = Chaos::Vec3(xpos, ypos, 0.f);
-				}
-			}
-		}
-	}
 };
 
+//entry point for the application
 class GameApp : public Chaos::Application
 {
 	public:
@@ -133,8 +46,6 @@ class GameApp : public Chaos::Application
 		
 	}
 };
-
-
 
 Chaos::Application* Chaos::CreateApplication()
 {
